9ca65aeb5eb4acbb8e3451da9015de4b42efbf9e
pratham-jaiswal <prathamj0502@gmail.com>
2023-05-11 03:56:10 +0530

    Fix editing functionality

--- public/js/script.js
+++ public/js/script.js
@@ -7,16 +7,6 @@ const fontSelect = document.getElementById('fontSelect');
 const sizeSelect = document.getElementById('sizeSelect');
 editor.focus();
 
-editor.addEventListener('input', () => {
-  const content = editor.innerHTML;
-  const noteId = '<%= note._id %>';
-  const url = `/notes/${noteId}`;
-  const xhr = new XMLHttpRequest();
-  xhr.open('PUT', url);
-  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-  xhr.send(JSON.stringify({content}));
-});
-
 boldButton.addEventListener('click', () => {
   document.execCommand('bold', false, null);
 });
