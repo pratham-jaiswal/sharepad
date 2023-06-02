9bc84b422df72cd551994799dbcf4c3ff87355ff
pratham-jaiswal <prathamj0502@gmail.com>
2023-06-02 22:19:31 +0530

    Add download functionality to save as .doc file

--- public/js/script.js
+++ public/js/script.js
@@ -43,3 +43,20 @@ fontSelect.addEventListener('change', () => {
 sizeSelect.addEventListener('change', () => {
   document.execCommand('fontSize', false, sizeSelect.value);
 });
+
+function saveDoc(){
+  var header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+       "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+       "xmlns='http://www.w3.org/TR/REC-html40'>"+
+       "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+  var footer = "</body></html>";
+  var sourceHTML = header+editor.innerHTML+footer;
+  
+  var source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+  var fileDownload = document.createElement("a");
+  document.body.appendChild(fileDownload);
+  fileDownload.href = source;
+  fileDownload.download = window.location.pathname.replace("/", "")+'.doc';
+  fileDownload.click();
+  document.body.removeChild(fileDownload);
+}
