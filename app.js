c19a0c1a613cfe9e5ff2f7cef4a4734a4688510e
pratham-jaiswal <prathamj0502@gmail.com>
2024-05-29 00:50:54 +0530

    Send emailjs variables

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const bcrypt = require("bcrypt");
-const geoip = require('geoip-lite');
+const geoip = require("geoip-lite");
 require("dotenv").config();
 
 app.set("view engine", "ejs");
@@ -10,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://pratham:"+process.env.PASSWORD+"@projects.aymrzso.mongodb.net/sharepadDB");
-// mongoose.connect("mongodb://localhost:27017/sharepadDB"); // for testing locally
+mongoose.connect(process.env.MONGODB_URL);
 
 const notesSchema = {
   name: {
@@ -63,7 +62,7 @@ app.post("/", async function (req, res) {
     lastAccessed: Date.now(),
     content: null,
     password: hashedPassword,
-  })
+  });
   newNote.save();
 
   if (password) {
@@ -85,7 +84,13 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  const currentYear = new Date().getFullYear();
+  res.render("contact.ejs", {
+    currentYear,
+    emailjsPublicKey: process.env.EMAIL_PUBLIC_KEY,
+    emailjsServiceKey: process.env.EMAIL_SERVICE_KEY,
+    emailjsTemplateId: process.env.EMAIL_TEMPLATE_ID,
+  });
 });
 
 app.post("/:noteName", async function (req, res) {
@@ -101,7 +106,7 @@ app.post("/:noteName", async function (req, res) {
   if (!findNote) {
     return res.status(404).render("404");
   }
-  
+
   if (findNote.password) {
     let password = req.body.password || "";
     error = "Please enter the correct password";
@@ -119,7 +124,7 @@ app.post("/:noteName", async function (req, res) {
 
 app.get("/:noteName", async function (req, res) {
   let { noteName } = req.params;
-  
+
   let findNote;
   try {
     findNote = await Note.findOne({ name: noteName }).exec();
@@ -138,8 +143,7 @@ app.get("/:noteName", async function (req, res) {
         name: noteName,
         error: "",
       });
-    }
-    else if (v != findNote.password) {
+    } else if (v != findNote.password) {
       return res.render("unlock", {
         name: noteName,
         error: "Please enter the correct password",
@@ -150,31 +154,17 @@ app.get("/:noteName", async function (req, res) {
   findNote.lastAccessed = Date.now();
   findNote.save();
 
-  let visitorIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress || "103.224.182.250";
-  let geoData = geoip.lookup(visitorIP); // geoip.lookup("103.224.182.250")
-
-  /* When testing on localhost visitor ip is ::1 so the timezone is not detected. 
-    Download ngrok from https://ngrok.com/download.
-    Extract ngrok.exe out from ngrok.zip.
-    Start nodejs application on localhost.
-    Double click on ngrok.exe and run the following command: ngrok http 3000
-    Then a link like this will be provided to you: https://a123-b234-c567.ngrok-free.app
-    Open the link in your browser, and your app is ready for testing.
-
-    OR
-
-    You can just provide a timezone like I did below: "Asia/Kolkata".
-
-    OR
-
-    You can just provide a custom ip like I did above: "103.224.182.250" (Location: Sydney, Australia).
-  */
+  let visitorIP =
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    "103.224.182.250";
+  let geoData = geoip.lookup(visitorIP);
 
   let timezone = (geoData && geoData.timezone) || "Asia/Kolkata";
 
   let expiry = new Date(findNote.lastAccessed);
   expiry.setDate(expiry.getDate() + 1);
-  let formattedExpiry = expiry.toLocaleString('en-IN', { timeZone: timezone});
+  let formattedExpiry = expiry.toLocaleString("en-IN", { timeZone: timezone });
 
   let content = findNote.content;
   res.render("notepad", {
@@ -196,11 +186,11 @@ app.put("/:noteName", async function (req, res) {
   } catch (err) {
     console.log(err);
   }
-  if(!findNote){
+  if (!findNote) {
     return res.status(404).render("404");
   }
   console.log(findNote.content);
-  console.log(req.body.content+"\n\n");
+  console.log(req.body.content + "\n\n");
 });
 
 app.use((req, res) => {
