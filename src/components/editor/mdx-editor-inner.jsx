5f3fb80537169851313fee5c297b2a63ae6d0fd2
pratham-jaiswal <prathamj0502@gmail.com>
2026-05-24 02:18:44 +0530

    use mdx editor instead of custom md editor

---
+++ src/components/editor/mdx-editor-inner.jsx
@@ -0,0 +1,163 @@
+"use client";
+
+import "@mdxeditor/editor/style.css";
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  linkPlugin,
+  linkDialogPlugin,
+  imagePlugin,
+  tablePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  toolbarPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  BlockTypeSelect,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  ListsToggle,
+  CodeToggle,
+  InsertCodeBlock,
+  TooltipWrap
+} from "@mdxeditor/editor";
+import { githubDark, githubLight } from "@uiw/codemirror-theme-github";
+import { toast } from "sonner";
+import { Copy } from "lucide-react";
+
+export function MDXEditorInner({ markdown, setMarkdown, dirtyRef }) {
+  const codeBlockLanguages = Object.fromEntries(
+    Object.entries({
+      txt: "Text",
+      bash: "Bash",
+      shell: "Shell",
+      sh: "Shell",
+      zsh: "Zsh",
+      fish: "Fish",
+
+      js: "JavaScript",
+      jsx: "JavaScript JSX",
+      ts: "TypeScript",
+      tsx: "TypeScript JSX",
+
+      html: "HTML",
+      css: "CSS",
+      scss: "SCSS",
+      sass: "Sass",
+      less: "Less",
+
+      json: "JSON",
+      yaml: "YAML",
+      yml: "YAML",
+      toml: "TOML",
+      xml: "XML",
+
+      md: "Markdown",
+
+      py: "Python",
+      rb: "Ruby",
+      php: "PHP",
+      java: "Java",
+      kotlin: "Kotlin",
+      scala: "Scala",
+      groovy: "Groovy",
+
+      c: "C",
+      cpp: "C++",
+      cs: "C#",
+      rs: "Rust",
+      go: "Go",
+
+      swift: "Swift",
+      dart: "Dart",
+
+      sql: "SQL",
+
+      dockerfile: "Dockerfile",
+
+      nginx: "Nginx",
+      apache: "Apache",
+
+      graphql: "GraphQL",
+
+      powershell: "PowerShell",
+
+      lua: "Lua",
+      perl: "Perl",
+      r: "R",
+      matlab: "MATLAB",
+
+      ini: "INI",
+
+      plaintext: "Plain Text",
+    }).sort(([a], [b]) => a.localeCompare(b)),
+  );
+
+  function copyMarkdown() {
+    navigator.clipboard.writeText(markdown);
+    toast.success("Markdown copied");
+  }
+
+  return (
+    <MDXEditor
+      markdown={markdown}
+      onChange={(value) => {
+        dirtyRef.current = true;
+        setMarkdown(value);
+      }}
+      contentEditableClassName="mdx-content"
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        imagePlugin(),
+        tablePlugin(),
+        codeBlockPlugin({
+          defaultCodeBlockLanguage: "txt",
+        }),
+        codeMirrorPlugin({
+          theme:
+            document.documentElement.getAttribute("data-theme") === "dark"
+              ? githubDark
+              : githubLight,
+
+          codeBlockLanguages: codeBlockLanguages,
+        }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <BlockTypeSelect />
+              <ListsToggle />
+              <CreateLink />
+              <InsertImage />
+              <InsertTable />
+              <CodeToggle />
+              <InsertCodeBlock />
+              <TooltipWrap title="Copy markdown">
+                <button
+                  type="button"
+                  className="mdx-toolbar-icon-btn"
+                  onClick={copyMarkdown}
+                  aria-label="Copy markdown"
+                >
+                  <Copy size={18} />
+                </button>
+              </TooltipWrap>
+            </>
+          ),
+        }),
+      ]}
+    />
+  );
+}
